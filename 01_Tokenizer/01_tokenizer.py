3b3ea0a1fece667ced005a992fe5b2c95aee06f6
yuan.feng <54296281@qq.com>
2024-04-22 17:48:27 +0800

    new

--- 01_Tokenizer/01_tokenizer.py
+++ 01_Tokenizer/01_tokenizer.py
@@ -3,17 +3,18 @@ from transformers import AutoTokenizer
 # model_id = 'openai-community/gpt2'
 # model_id= 'google-bert/bert-base-cased'
 # 'facebook/mbart-large-cc25'
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf', token='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')
 
-# The first sentences from the abstract of "<Attention Is All You Need>"
+model_path = '/Users/yuan.feng/PycharmProjects/From_Transformer_to_GPTs/01_Tokenizer/llama-2'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
 corpus = [
     "The dominant sequence transduction models are based on complex recurrent or convolutional neural networks that include an encoder and a decoder.",
     "主导的序列转换模型基于复杂的循环或卷积神经网络，包括一个编码器和一个解码器。"]
 
 result = tokenizer(corpus)
-print(result['input_ids'][1])
-for i in result['input_ids'][1]:
-    print(tokenizer.decode(i).encode('utf-8'))
+print(result['input_ids'][0])
+for i in result['input_ids'][0]:
+    print(tokenizer.decode(i))
 
 print(tokenizer.all_special_tokens)
 print(tokenizer.__len__())
