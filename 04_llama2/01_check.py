02daafd44ef8e17865fbb4c06f191f949b930e5f
yuan.feng <54296281@qq.com>
2024-05-24 17:19:25 +0800

    mac

--- 04_llama2/01_check.py
+++ 04_llama2/01_check.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
+from utils import init_model
 from llama import *
+import matplotlib.pyplot as plt
 from torch import Tensor
 import torch
 from modelsummary import summary
@@ -8,6 +9,7 @@ from chatglm_tokenizer.tokenization_chatglm import ChatGLMTokenizer
 import yaml
 
 
+# ---------------------------llama--------------------------------- #
 def check_rope():
     class PositionalEncoding(nn.Module):
         def __init__(self, max_len: int, d_model: int, dropout: float, batch_first=False):
@@ -154,6 +156,29 @@ def check_feed_forward():
     # todo
 
 
+def check_model_and_loss():
+    model_args: ModelArgs = ModelArgs(
+        max_seq_len=MAX_SEQ_LEN,
+        max_batch_size=MAX_BATCH_SIZE,
+        dim=D_MODEL,
+        n_layers=N_LAYER,
+        n_heads=N_HEAD,
+        vocab_size=VOCAB_SIZE,
+        device=DEVICE
+    )
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-1)
+    model = LlamaModel(model_args).to(DEVICE)
+    tokens = torch.randint(low=0, high=100, size=(BATCH_SIZE, SEQ_LEN), dtype=torch.long)
+    summary(model, tokens, 0, show_input=True)
+    output = model(tokens, 0)
+
+    assert output.shape == (BATCH_SIZE, SEQ_LEN, VOCAB_SIZE)
+    loss = loss_fn(output.reshape(-1, output.shape[-1]), tokens.reshape(-1))
+    print(loss)
+
+
+# ---------------------------tokenizer--------------------------------- #
+
 def check_tokenizer():
     tokenizer = SentencePieceProcessor()
     with open('../00_assets/yml/local_settings.yml', 'r') as file:
@@ -184,99 +209,6 @@ def check_glm_tokenizer():
     print('eos_token_id:', tokenizer.special_tokens['<eos>'])
 
 
-def check_model_and_loss():
-    model_args: ModelArgs = ModelArgs(
-        max_seq_len=MAX_SEQ_LEN,
-        max_batch_size=MAX_BATCH_SIZE,
-        dim=D_MODEL,
-        n_layers=N_LAYER,
-        n_heads=N_HEAD,
-        vocab_size=VOCAB_SIZE,
-        device=DEVICE
-    )
-    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-1)
-    model = LlamaModel(model_args).to(DEVICE)
-    tokens = torch.randint(low=0, high=100, size=(BATCH_SIZE, SEQ_LEN), dtype=torch.long)
-    summary(model, tokens, 0, show_input=True)
-    output = model(tokens, 0)
-
-    assert output.shape == (BATCH_SIZE, SEQ_LEN, VOCAB_SIZE)
-    loss = loss_fn(output.reshape(-1, output.shape[-1]), tokens.reshape(-1))
-    print(loss)
-
-
-def check_sliding_window_attention():
-    print_order = ['the', 'cat', 'is', 'on', 'a', 'chair']
-    sequence = [{print_order[i]} for i in range(len(print_order))]
-    sliding_window_size = 3
-
-    def sliding_window_attention(seq: list[set[str]], w: int):
-        seq_len = len(seq)
-        attention_scores: list[list[set]] = [[None for _ in range(seq_len)] for _ in range(seq_len)]
-        for i, q_tokens_set in enumerate(seq):
-            for j, k_tokens_set in enumerate(seq):
-                # The upper triangle is all None
-                if j > i:
-                    continue
-                # Each token can only attend to the previous W tokens
-                if i - j >= w:
-                    continue
-
-                attention = set()
-                # Add all tokens from q_tokens_set to attention_result
-                attention.update(q_tokens_set)
-                # Add all tokens from k_tokens_set to attention_resul
-                attention.update(k_tokens_set)
-
-                attention_scores[i][j] = attention
-        return attention_scores
-
-    def multiple_by_v(attention_scores: list[list[set]], v_sequence: list[set[str]]) -> list[set[str]]:
-        seq_len = len(v_sequence)
-        result = [set() for _ in range(seq_len)]
-        for i in range(seq_len):
-            for j in range(seq_len):
-                attention = attention_scores[i][j]
-                v = v_sequence[j]
-                r = result[i]
-                # Add all the tokens in the attention (if not None) to r
-                if attention is not None:
-                    # Add all the tokens in v to r
-                    r.update(v)
-                    r.update(attention)
-        return result
-
-    def print_attention(attention_scores: list[list[set[str]]]):
-        for i, row in enumerate(attention_scores):
-            for j, attention in enumerate(row):
-                if attention is None:
-                    print('None', end='\t')
-                else:
-                    print(f'{sorted(attention, key=lambda x: print_order.index(x))}', end='\t')
-            print()
-
-    def print_sequence(seq: list[set[str]]):
-        for i, tokens_set in enumerate(seq):
-            print(f'{i}: {sorted(tokens_set, key=lambda x: print_order.index(x))}')
-
-    def print_layer(input: list[set[str]], layer_num: int) -> list[set[str]]:
-        print(f'Layer {layer_num} input:')
-        print_sequence(input)
-        attention_scores = sliding_window_attention(input, sliding_window_size)
-        print()
-        print(f'Layer {layer_num} attention scores:')
-        print_attention(attention_scores)
-        output = multiple_by_v(attention_scores, input)
-        print()
-        print(f'Layer {layer_num} output:')
-        print_sequence(output)
-        return output
-
-    output_layer_1 = print_layer(sequence, 1)
-    output_layer_2 = print_layer(output_layer_1, 2)
-    output_layer_3 = print_layer(output_layer_2, 3)
-
-
 if __name__ == '__main__':
     VOCAB_SIZE = 32000
     SEQ_LEN = 23
@@ -293,8 +225,9 @@ if __name__ == '__main__':
     # check_rope()
     # check_rms_norm()
     # check_silu()
-    check_kv_cache()
+    # check_kv_cache()
     # check_feed_forward()
     # # check_tokenizer()
     # check_glm_tokenizer()
     # check_model_and_loss()
+
--- 04_llama2/03_tiny_pretrain.py
+++ 04_llama2/03_tiny_pretrain.py
@@ -9,6 +9,8 @@ from torch.utils.data import Dataset
 import torch
 import os
 import yaml
+from torch.optim import AdamW
+from transformers import get_cosine_schedule_with_warmup
 
 '''
 https://github.com/DLLXW/baby-llama2-chinese
@@ -52,28 +54,6 @@ def process_wiki_clean(path_in, path_out):
         f.write(arr.tobytes())
 
 
-def train_epoch(model, dataloader):
-    model.train()
-    losses = 0
-    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'],
-                                 betas=(config['beta1'], config['beta2']))
-    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=0)
-
-    for i, d in enumerate(dataloader):
-        src = d[:, :-1].to(config['device'])
-        tgt = d[:, 1:].to(config['device'])
-        tgt_predict = model(src, 0)
-
-        optimizer.zero_grad()
-        loss = loss_fn(tgt_predict.reshape(-1, tgt_predict.shape[-1]), tgt.reshape(-1))
-        loss.backward()
-        optimizer.step()
-        losses += loss.item()
-        pbar.update(1)
-
-    return losses / len(list(dataloader))
-
-
 if __name__ == "__main__":
     # 模型训练不能用kv_cache，因为...
     with open('../00_assets/yml/tiny_chinese_llama.yml', 'r') as file:
@@ -93,20 +73,53 @@ if __name__ == "__main__":
         print('-------data process--------')
         process_wiki_clean(data_input, data_output)
 
-    train_ds = PretrainDataset([data_output], max_length=config['max_seq_len'])
-    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=config['batch_size'])
+    train_loader = torch.utils.data.DataLoader(PretrainDataset([data_output], max_length=config['max_seq_len']),
+                                               batch_size=config['batch_size'])
     llama_model = init_model(config)
+
+    # 设置优化器和学习率调度器的参数
+    learning_rate = 2e-5
+    weight_decay = 0.1
+    beta1 = 0.9
+    beta2 = 0.95
+    eps = 1e-5
+    warmup_steps = 200
+    gradient_clipping_value = 1.0
+    total_training_steps = len(train_loader) * config['num_epochs']
+    optimizer = AdamW(llama_model.parameters(),
+                      lr=learning_rate, betas=(beta1, beta2), eps=eps, weight_decay=weight_decay)
+    scheduler = get_cosine_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=warmup_steps,
+                                                num_training_steps=total_training_steps)
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=0)
 
     print("Total trainable parameters:", sum(p.numel() for p in llama_model.parameters() if p.requires_grad))
 
     min_train_loss = float('inf')
+
     for epoch in range(1, config['num_epochs'] + 1):
         start_time = timer()
         with tqdm(total=len(list(train_loader)), desc=f'Epoch {epoch}', unit='batch') as pbar:
-            train_loss = train_epoch(llama_model, train_loader)
+            llama_model.train()
+            losses = 0
+            for i, data in enumerate(train_loader):
+                src = data[:, :-1].to(config['device'])
+                tgt = data[:, 1:].to(config['device'])
+                tgt_predict = llama_model(src, 0).to(config['device'])
+
+                optimizer.zero_grad()
+                loss = loss_fn(tgt_predict.reshape(-1, tgt_predict.shape[-1]), tgt.reshape(-1))
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(llama_model.parameters(), gradient_clipping_value)
+                optimizer.step()
+                scheduler.step()
+                losses += loss.item()
+                pbar.update(1)
+
+            train_loss = losses / len(list(train_loader))
+
             if train_loss < min_train_loss:
                 min_train_loss = train_loss
                 torch.save(llama_model.state_dict(), '{}/best.pth'.format(save_dir))
             end_time = timer()
-            print((f"Epoch: {epoch}, Train loss: {train_loss:.3f}, "
-                   f"Epoch time = {(end_time - start_time):.3f}s"))
+            print(f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Epoch time = {(end_time - start_time):.3f}s")
