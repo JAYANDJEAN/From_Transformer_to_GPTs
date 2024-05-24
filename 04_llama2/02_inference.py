96d3e63a80b4ae8ad9874dc9f573d47a5d54d311
fengyuan <54296281@qq.com>
2024-05-24 22:56:57 +0800

    mac

--- 04_llama2/02_inference.py
+++ 04_llama2/02_inference.py
@@ -10,8 +10,8 @@ prompts = data['prompts']
 torch.manual_seed(0)
 with open('../00_assets/yml/local_settings.yml', 'r') as file:
     setting = yaml.safe_load(file)
-device = "cpu"  # 目前m3 max上的cpu可跑，还未测试cuda，mps报错，未细看
-model_type = 'tiny'  # '7b', 'hf'
+device = "cpu"  # 目前m3 max上的cpu可跑，cuda，mps均报错
+model_type = 'tiny'  # 7b, hf, tiny
 
 # 两种生成出来的还是有些不一样的，我也没查具体原因。
 if model_type == 'hf':
