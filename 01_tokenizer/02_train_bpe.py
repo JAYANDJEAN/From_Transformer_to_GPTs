85c492ed88b186f3fed9311e2f16bed14c728cf1
fengyuan <54296281@qq.com>
2024-05-26 08:21:26 +0800

    mac

--- 01_tokenizer/02_train_bpe.py
+++ 01_tokenizer/02_train_bpe.py
@@ -1,27 +1,12 @@
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
-from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
 
-# 应该有bug，但还没检查出来
-tokenizer = Tokenizer(BPE())
-tokenizer.pre_tokenizer = Whitespace()
+# tokenizers 不是用python实现的
+
+tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+
 trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-tokenizer.train(files=["../00_assets/train.de", "../00_assets/train.en"], trainer=trainer)
 
-corpus = [
-    "Ein Mann in grün hält eine Gitarre, während der andere Mann sein Hemd ansieht.",
-    "A man in green holds a guitar 中 😁 while the other man observes his shirt."
-]
-
-print(tokenizer.get_vocab_size())
-print(tokenizer.id_to_token(0))
-print(tokenizer.id_to_token(1))
-print(tokenizer.id_to_token(2))
-output = tokenizer.encode("Hello, y'all! How are you 😁 ?")
-print(output.tokens)
-
-for sentence in corpus:
-    res = tokenizer.encode(sentence)
-    print(res.ids)
-    print(res.tokens)
+tokenizer.pre_tokenizer = Whitespace()
