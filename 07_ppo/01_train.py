5d4ede4745aeb6e782394b31850a2747ad9570e2
fengyuan <54296281@qq.com>
2024-07-10 11:29:40 +0800

    mac

--- 07_ppo/01_train.py
+++ 07_ppo/01_train.py
@@ -5,6 +5,9 @@ import yaml
 import glob
 from PIL import Image
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 with open('../00_assets/yml/ppo_trainer.yml', 'r') as file:
     config = yaml.safe_load(file)
 
@@ -178,6 +181,92 @@ def visual():
     img.save(fp=gif_path, format='GIF', append_images=imgs, save_all=True,
              optimize=True, duration=frame_duration, loop=0)
     print("saved gif at : ", gif_path)
+
+
+def reward_graph():
+    print("=" * 90)
+    plot_avg = True  # plot average of all runs; else plot all runs separately
+    fig_width = 10
+    fig_height = 6
+
+    window_len_smooth = 20
+    min_window_len_smooth = 1
+    linewidth_smooth = 1.5
+    alpha_smooth = 1
+    window_len_var = 5
+    min_window_len_var = 1
+    linewidth_var = 2
+    alpha_var = 0.1
+    colors = ['red', 'blue', 'green', 'orange', 'purple', 'olive', 'brown',
+              'magenta', 'cyan', 'crimson', 'gray', 'black']
+
+    # make directory for saving figures
+
+    figure_dir = config['figure_dir'] + '/' + config['env_name'] + '/'
+    log_dir = config['log_dir'] + '/' + config['env_name'] + '/'
+
+    os.makedirs(config['figure_dir'], exist_ok=True)
+    os.makedirs(figure_dir, exist_ok=True)
+    fig_save_path = figure_dir + f"fig_{config['env_name'].lower()}_{config['random_seed']}.png"
+    current_num_files = next(os.walk(log_dir))[2]
+    num_runs = len(current_num_files)
+    all_runs = []
+
+    for run_num in range(num_runs):
+        log_f_name = log_dir + f"log_{config['env_name'].lower()}_{run_num}.csv"
+        print("loading data from : " + log_f_name)
+        data = pd.read_csv(log_f_name)
+        data = pd.DataFrame(data)
+        print("data shape : ", data.shape)
+        all_runs.append(data)
+        print("-" * 80)
+
+    ax = plt.gca()
+
+    if plot_avg:
+        df_concat = pd.concat(all_runs)
+        df_concat_groupby = df_concat.groupby(df_concat.index)
+        data_avg = df_concat_groupby.mean()
+        data_avg['reward_smooth'] = data_avg['reward'].rolling(window=window_len_smooth, win_type='triang',
+                                                               min_periods=min_window_len_smooth).mean()
+        data_avg['reward_var'] = data_avg['reward'].rolling(window=window_len_var, win_type='triang',
+                                                            min_periods=min_window_len_var).mean()
+
+        data_avg.plot(kind='line', x='timestep', y='reward_smooth', ax=ax, color=colors[0], linewidth=linewidth_smooth,
+                      alpha=alpha_smooth)
+        data_avg.plot(kind='line', x='timestep', y='reward_var', ax=ax, color=colors[0], linewidth=linewidth_var,
+                      alpha=alpha_var)
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend([handles[0]], ["reward_avg_" + str(len(all_runs)) + "_runs"], loc=2)
+    else:
+        for i, run in enumerate(all_runs):
+            # smooth out rewards to get a smooth and a less smooth (var) plot lines
+            run['reward_smooth_' + str(i)] = run['reward'].rolling(window=window_len_smooth, win_type='triang',
+                                                                   min_periods=min_window_len_smooth).mean()
+            run['reward_var_' + str(i)] = run['reward'].rolling(window=window_len_var, win_type='triang',
+                                                                min_periods=min_window_len_var).mean()
+            run.plot(kind='line', x='timestep', y='reward_smooth_' + str(i), ax=ax, color=colors[i % len(colors)],
+                     linewidth=linewidth_smooth, alpha=alpha_smooth)
+            run.plot(kind='line', x='timestep', y='reward_var_' + str(i), ax=ax, color=colors[i % len(colors)],
+                     linewidth=linewidth_var, alpha=alpha_var)
+        handles, labels = ax.get_legend_handles_labels()
+        new_handles = []
+        new_labels = []
+        for i in range(len(handles)):
+            if i % 2 == 0:
+                new_handles.append(handles[i])
+                new_labels.append(labels[i])
+        ax.legend(new_handles, new_labels, loc=2)
+    ax.grid(color='gray', linestyle='-', linewidth=1, alpha=0.2)
+    ax.set_xlabel("Timesteps", fontsize=12)
+    ax.set_ylabel("Rewards", fontsize=12)
+    plt.title(config['env_name'], fontsize=14)
+    fig = plt.gcf()
+    fig.set_size_inches(fig_width, fig_height)
+    print("=" * 90)
+    plt.savefig(fig_save_path)
+    print("figure saved at : ", fig_save_path)
+    plt.show()
 
 
 if __name__ == '__main__':
