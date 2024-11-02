d29a61cfae33af01dd973d076ccb320c386abef6
Jay <54296281@qq.com>
2024-11-02 21:03:03 +0800

    Update 03_tiny_pretrain.py

--- 04_llama2/03_tiny_pretrain.py
+++ 04_llama2/03_tiny_pretrain.py
@@ -1,55 +1,34 @@
-from tqdm import tqdm
-from timeit import default_timer as timer
-from utils import init_model, process_wiki_clean, PretrainDataset, ChatGLMTokenizer
-from torch.utils.data import Dataset
-import torch
 import os
-import yaml
+from timeit import default_timer as timer
+
+import torch
 from torch.optim import AdamW
+from torch.utils.data import Dataset
+from tqdm import tqdm
 from transformers import get_cosine_schedule_with_warmup
+
+from utils import init_model, PretrainDataset, ChatGLMTokenizer
 
 '''
 https://github.com/DLLXW/baby-llama2-chinese
 '''
 
-if __name__ == "__main__":
-    # 模型训练不能用kv_cache，因为...
-    with open('../00_assets/yml/tiny_chinese_llama.yml', 'r') as file:
-        config = yaml.safe_load(file)
-    with open('../00_assets/yml/local_settings.yml', 'r') as file:
-        setting = yaml.safe_load(file)
 
+def train_and_translate(config):
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     tokenizer = ChatGLMTokenizer(vocab_file='../00_assets/tokenizers/chatglm_sentencepiece/tokenizer.model')
-    data_input = setting['model_path'] + 'wikipedia-cn-20230720-filtered.json'
-    data_output = setting['model_path'] + 'wiki.bin'
-    save_dir = os.path.join(setting['model_path'], 'tiny_llama')
-    config['save_dir'] = save_dir
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    if not os.path.exists(data_output):
-        print('-------data process--------')
-        process_wiki_clean(data_input, data_output, tokenizer)
-
-    train_loader = torch.utils.data.DataLoader(PretrainDataset([data_output], max_length=config['max_seq_len']),
-                                               batch_size=config['batch_size'])
+    train_loader = torch.utils.data.DataLoader(
+        PretrainDataset(["../00_assets/data/wiki.bin"],
+                        max_length=config['max_seq_len']),
+        batch_size=config['batch_size'])
     llama_model = init_model(config)
-
-    # 设置优化器和学习率调度器的参数
-    learning_rate = 2e-5
-    weight_decay = 0.1
-    beta1 = 0.9
-    beta2 = 0.95
-    eps = 1e-5
-    warmup_steps = 200
     gradient_clipping_value = 1.0
-    total_training_steps = len(train_loader) * config['num_epochs']
     optimizer = AdamW(llama_model.parameters(),
-                      lr=learning_rate, betas=(beta1, beta2), eps=eps, weight_decay=weight_decay)
+                      lr=config['lr'], betas=(config['beta1'], config['beta2']),
+                      eps=config['eps'], weight_decay=config['weight_decay'])
     scheduler = get_cosine_schedule_with_warmup(optimizer,
-                                                num_warmup_steps=warmup_steps,
-                                                num_training_steps=total_training_steps)
+                                                num_warmup_steps=config['warmup_steps'],
+                                                num_training_steps=len(train_loader) * config['num_epochs'])
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
     print("Total trainable parameters:", sum(p.numel() for p in llama_model.parameters() if p.requires_grad))
 
@@ -60,10 +39,9 @@ if __name__ == "__main__":
             llama_model.train()
             losses = 0
             for i, train_data in enumerate(train_loader):
-                src = train_data[:, :-1].to(config['device'])
-                tgt = train_data[:, 1:].to(config['device'])
-                tgt_predict = llama_model(src, 0).to(config['device'])
-
+                src = train_data[:, :-1].to(device)
+                tgt = train_data[:, 1:].to(device)
+                tgt_predict = llama_model(src, 0).to(device)
                 optimizer.zero_grad()
                 loss = loss_fn(tgt_predict.reshape(-1, tgt_predict.shape[-1]), tgt.reshape(-1))
                 loss.backward()
@@ -72,11 +50,37 @@ if __name__ == "__main__":
                 scheduler.step()
                 losses += loss.item()
                 pbar.update(1)
-
             train_loss = losses / len(list(train_loader))
 
             if train_loss < min_train_loss:
                 min_train_loss = train_loss
-                torch.save(llama_model.state_dict(), '{}/tiny_chinese_llama.pth'.format(save_dir))
+                torch.save(llama_model.state_dict(), f"{config['save_dir']}/tiny_chinese_llama.pth")
             end_time = timer()
             print(f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Epoch time = {(end_time - start_time):.3f}s")
+
+
+if __name__ == "__main__":
+    # 模型训练不能用kv_cache，因为...
+    train_config = {
+        'init_from': 'scratch',
+        'save_dir': '../00_assets/models',
+        # 'dtype': float16,
+        'batch_size': 32,
+        'num_epochs': 10,
+        'dim': 256,
+        'n_layers': 2,
+        'n_heads': 4,
+        'n_kv_heads': 4,
+        'vocab_size': 64793,
+        'max_seq_len': 256,
+        'hidden_dim': 512,
+        'dropout': 0.0,
+        'lr': 2e-5,
+        'beta1': 0.9,
+        'beta2': 0.98,
+        'eps': 1e-5,
+        'weight_decay': 0.1,
+        'warmup_steps': 200
+    }
+
+    os.makedirs(train_config['save_dir'], exist_ok=True)
